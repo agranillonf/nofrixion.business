77325216beddab79b3225ca2a3a877e03844234b
sauravmaiti22 <103389834+sauravmaiti22@users.noreply.github.com>
2024-01-23 10:54:37 +0000

    Using predefined classname for duration

--- packages/components/src/components/ui/TableScrollbar/TableScrollbar.tsx
+++ packages/components/src/components/ui/TableScrollbar/TableScrollbar.tsx
@@ -19,7 +19,7 @@ const TableScrollbar = forwardRef(
           <RadixScrollArea.Scrollbar
             ref={ref}
             className={classNames('flex select-none overflow-hidden touch-non', {
-              'rounded-2xl bg-[#F6F8F9] transition-colors duration-[100ms] ease-out hover:bg-[#EAF0F1] flex-col h-4':
+              'rounded-2xl bg-[#F6F8F9] transition-colors duration-100 ease-out hover:bg-[#EAF0F1] flex-col h-4':
                 !props.hideScrollbar,
             })}
             orientation="horizontal"
@@ -32,7 +32,7 @@ const TableScrollbar = forwardRef(
                   height: '16px',
                 }}
                 className={cn(
-                  'hover:border-[#EAF0F1] hover:bg-[#73808C] border-[6px] border-[#F6F8F9] bg-[#ABB2BA] rounded-lg my-auto transition-colors duration-[100ms] ease-out',
+                  'hover:border-[#EAF0F1] hover:bg-[#73808C] border-[6px] border-[#F6F8F9] bg-[#ABB2BA] rounded-lg my-auto transition-colors duration-100 ease-out',
                   { 'border-[#EAF0F1] bg-[#73808C]': isTrackHovered },
                 )}
               ></div>
